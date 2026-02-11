5e4e7dfc6bc7593bf775482b2d75f0d0931a3f97
Ash <ash@greenwoodpark.com.au>
2026-02-11 14:02:06 +1030

    Initial Commit

---
+++ ClubResultsTests/ClubResultsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClubResultsTests.swift
+//  ClubResultsTests
+//
+//  Created by Ashley Williams on 11/2/2026.
+//
+
+import Testing
+@testable import ClubResults
+
+struct ClubResultsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
